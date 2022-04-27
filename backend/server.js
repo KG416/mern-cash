caa30be83b4243d4892227c435bc358a88f1f1fb
karlG <kallegunnarsson@hotmail.com>
2022-04-27 13:34:08 +0200

    🗃️ connect db + 📝 update readme

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 
+const connectDB = require('./config/db')
 const postRoutes = require('./routes/postRoutes')
 const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
+connectDB()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
