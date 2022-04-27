0f6416126d4e8a999a00d7ca0ebbd36cc41c5ac7
karlG <kallegunnarsson@hotmail.com>
2022-04-27 11:48:31 +0200

    add express-async-handler + setup controller & routes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,18 +1,24 @@
-const getPosts = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getPosts = async (req, res) => {
     res.status(200).json('get all')
 }
 
-const setPost = (req, res) => {
+const setPost = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add text field')
+    }
     res.status(200).json('create')
-}
+})
 
-const updatePost = (req, res) => {
+const updatePost = asyncHandler(async (req, res) => {
     res.status(200).json(`update ${req.params.id}`)
-}
+})
 
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
     res.status(200).json(`delete ${req.params.id}`)
-}
+})
 
 module.exports = {
     getPosts,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const postRoutes = require('./routes/postRoutes')
 
-const PORT = process.env.PORT || 5000
+const postRoutes = require('./routes/postRoutes')
+const { errorHandler } = require('./middleware/errorMiddleware')
+
 const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/posts', postRoutes)
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
